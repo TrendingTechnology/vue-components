b6d2fc882d3c1107e10e2d1b8d7bdc8a93719f48
Divyam Rastogi <divyamr@flock.com>
2018-06-18 19:46:34 +0530

    Inlining SVGs.

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,25 @@ const fs = require('fs');
 
 module.exports = {
   lintOnSave: false,
+  chainWebpack: (config) => {
+    // remove the old loader
+    config.module.rules.delete('svg');
+  },
+  configureWebpack: {
+    module: {
+      rules: [{
+        test: /\.(svg)(\?.*)?$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+            },
+          },
+        ],
+      }],
+    },
+  },
   css: {
     extract: false,
     loaderOptions: {
